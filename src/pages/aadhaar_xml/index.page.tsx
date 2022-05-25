d124f445228ecf45d2836bea1d244300a5120b4a
amirsarfraz <amirsarfarz50@gmail.com>
2022-05-25 20:50:10 +0500

    radio button

--- src/pages/aadhaar_xml/index.page.tsx
+++ src/pages/aadhaar_xml/index.page.tsx
@@ -10,7 +10,7 @@ import { GreaterThenIcon } from '@/assets/svg/greaterthen-icon';
 import { AadhaarXmlSvg } from '@/assets/svg/aadhaar_xml';
 // import RadioInputStyled from '@/components/core/RadioInput';
 import MyCommenceCenteredModal from '@/components/core/CommenceVideomodel/index.page';
-import { DivMain, DivSvg, FooterButtonStyle, DivForm, FormLabel, MainStyle } from './index.styles';
+import { DivMain, DivSvg, FooterButtonStyle, DivForm, FormLabel, MainStyle, YesButtonStyle } from './index.styles';
 /**
  *
  * @returns AadhaarXml page
@@ -42,44 +42,28 @@ const AadhaarXml = () => {
           <FormLabel>Do you have an Aadhaar XML file?</FormLabel>
         </DivForm>
         <MainStyle isChecked={ischecked}>
-          <form action="#" className="customRadio customCheckbox m-0 p-0">
-            <div className="row mb-0">
-              <div className="row justify-content-start">
-                <div className="col-12">
-                  <div className="row">
-                    <input
-                      type="radio"
-                      name="textEditor"
-                      id="dreamweaver"
-                      checked
-                      onClick={() => (!ischecked ? setIsChecked(true) : setIsChecked(false))}
-                      className={ischecked ? 'checkclass' : 'notcheck'}
-                    />
-                    <label htmlFor="dreamweaver">Yes</label>
-                  </div>
-                  <div className="row">
-                    <input type="radio" name="textEditor" id="sublime" />
-                    <label htmlFor="sublime">No</label>
-                  </div>
-                </div>
-              </div>
+          <form action="#" className="customRadio">
+            <div className="row">
+              <input
+                type="radio"
+                name="textEditor"
+                id="dreamweaver"
+                checked
+                onClick={() => (ischecked ? setIsChecked(true) : setIsChecked(false))}
+                // className={ischecked ? 'checkclass' : 'notcheck'}
+              />
+              <label htmlFor="dreamweaver">
+                <YesButtonStyle>Yes</YesButtonStyle>
+              </label>
+            </div>
+            <div className="row">
+              <input type="radio" name="textEditor" id="sublime" />
+              <label htmlFor="sublime">
+                <YesButtonStyle>No</YesButtonStyle>
+              </label>
             </div>
           </form>
           <div></div>
-          <form>
-            <fieldset>
-              <div>
-                <input type="radio" id="contactChoice1" name="contact" value="email" checked />
-                <label htmlFor="contactChoice1">Email</label>
-
-                <input type="radio" id="contactChoice2" name="contact" value="phone" />
-                <label htmlFor="contactChoice2">Phone</label>
-
-                <input type="radio" id="contactChoice3" name="contact" value="mail" />
-                <label htmlFor="contactChoice3">Mail</label>
-              </div>
-            </fieldset>
-          </form>
         </MainStyle>
       </div>
 
